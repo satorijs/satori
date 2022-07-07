c790afae9b4d248056e6f831043b3bf9a0fcc589
Shigma <shigma10826@gmail.com>
2022-07-08 02:24:16 +0800

    feat(qqguild): support qqguild adapter

---
+++ adapters/qqguild/src/utils.ts
@@ -0,0 +1,15 @@
+import { Guild, User } from '@satorijs/core'
+import * as QQGuild from '@qq-guild-sdk/core'
+
+export const adaptGuild = (guild: QQGuild.Guild): Guild => ({
+  guildId: guild.id,
+  guildName: guild.name,
+})
+
+export const adaptUser = (user: QQGuild.User): User => ({
+  isBot: user.bot,
+  avatar: user.avatar,
+  userId: user.id,
+  username: user.username,
+  nickname: user.username,
+})
---
+++ adapters/qqguild/src/ws.ts
@@ -0,0 +1,31 @@
+import { Adapter } from '@satorijs/core'
+import { Schema } from '@satorijs/env-node'
+import { QQGuildBot } from './bot'
+
+export class WsClient extends Adapter<QQGuildBot> {
+  async start(bot: QQGuildBot) {
+    Object.assign(bot, await bot.getSelf())
+    await bot.internal.startClient(bot.config.intents)
+    bot.internal.on('ready', bot.online.bind(bot))
+    bot.internal.on('message', msg => {
+      const session = bot.adaptMessage(msg)
+      if (session) {
+        session.type = 'message'
+        bot.dispatch(session)
+      }
+    })
+  }
+
+  async stop(bot: QQGuildBot) {
+    bot.internal.stopClient()
+    bot.offline()
+  }
+}
+
+export namespace WsClient {
+  export interface Config extends Adapter.WsClient.Config {}
+
+  export const Config: Schema<Config> = Schema.intersect([
+    Adapter.WsClient.Config,
+  ])
+}
