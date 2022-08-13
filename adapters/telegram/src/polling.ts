c08b64ffbe349d95a50768f98a438b5c18674c9b
samnyan <4137880+samnyan@users.noreply.github.com>
2022-08-14 01:34:02 +0900

    feat(telegram): Add reconnect interval.

--- adapters/telegram/src/polling.ts
+++ adapters/telegram/src/polling.ts
@@ -10,6 +10,9 @@ export class HttpPolling extends Adapter.Client<TelegramBot> {
 
   async start(bot: TelegramBot<Context, TelegramBot.BaseConfig & HttpPolling.Config>) {
     bot.initialize(async () => {
+      let _retryCount = 0
+      let _initial = true
+      const { retryTimes, retryInterval } = bot.config
       const { url } = await bot.internal.getWebhookInfo()
       if (url) {
         logger.warn('Bot currently has a webhook set up, trying to remove it...')
@@ -36,26 +39,32 @@ export class HttpPolling extends Adapter.Client<TelegramBot> {
             timeout: bot.config.pollingTimeout,
           })
           bot.online()
+          _retryCount = 0
+          _initial = false
+          
           for (const e of updates) {
             this.offset = Math.max(this.offset, e.update_id)
             handleUpdate(e, bot)
           }
+          setTimeout(polling, 0)
         } catch (e) {
-          bot.status = 'reconnect'
-          // Telegram error
-          if (axios.isAxiosError(e) && e.response && e.response.data) {
-            const errorBody = e.response.data
-            logger.warn('failed to get updates: (%c) %s', errorBody.error_code, errorBody.description)
-            // Handle Bot Conflict
-            if (errorBody.error_code == 409) {
-              bot.status = 'disconnect'
-            }
-          } else {
+          if (!axios.isAxiosError(e) || !e.response?.data) {
             // Other error
             logger.warn('failed to get updates. reason: %s', e.message)
+          } else {
+            // Telegram error
+            const errorBody = e.response.data
+            logger.warn('failed to get updates: (%c) %s', errorBody.error_code, errorBody.description)
           }
+
+          if (_initial && _retryCount > retryTimes) {
+            bot.error = e
+            return bot.status = 'offline'
+          }
+          _retryCount++
+          bot.status = 'reconnect'
+          setTimeout(() => polling(), retryInterval)
         }
-        setTimeout(polling, 0)
       }
       polling()
       logger.debug('listening updates %c', 'telegram: ' + bot.selfId)
@@ -69,10 +78,14 @@ export namespace HttpPolling {
   export interface Config {
     protocol: 'polling'
     pollingTimeout?: number
+    retryTimes?: number
+    retryInterval?: number
   }
 
   export const Config: Schema<Config> = Schema.object({
     protocol: Schema.const('polling' as const).required(),
     pollingTimeout: Schema.natural().role('time').default(Time.minute).description('通过长轮询获取更新时请求的超时 (单位为秒)。'),
+    retryTimes: Schema.natural().description('初次连接时的最大重试次数。').default(6),
+    retryInterval: Schema.natural().role('ms').default(Time.second * 5).description('长轮询断开后的重试时间间隔 (单位为毫秒)。'),
   })
 }
