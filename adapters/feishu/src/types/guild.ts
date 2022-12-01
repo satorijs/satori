d3e2e4552297711bbf30575c55e11de949b82146
Maiko Tan <maiko.tan.coding@gmail.com>
2022-12-01 14:12:49 +0800

    fix: add comment to doc link

--- adapters/feishu/src/types/guild.ts
+++ adapters/feishu/src/types/guild.ts
@@ -40,8 +40,11 @@ export interface GuildMember {
 
 declare module './internal' {
   export interface Internal {
+    /** @see https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/chat/list */
     getCurrentUserGuilds(params: Pagination<{ user_id_type: Feishu.UserIdType }>): Promise<{ data: Paginated<Feishu.Guild> }>
+    /** @see https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/chat/get */
     getGuildInfo(chat_id: string, params: { user_id_type: string }): Promise<BaseResponse & { data: Feishu.Guild }>
+    /** @see https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/chat-members/get */
     getGuildMembers(chat_id: string, params: Pagination<{ member_id_type: Feishu.UserIdType }>): Promise<{ data: Paginated<GuildMember> }>
   }
 }
