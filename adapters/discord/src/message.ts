6b3b476a66d2f9b0f72ca3037158367e6bd0c8d0
Shigma <shigma10826@gmail.com>
2022-12-08 00:01:40 +0800

    Update adapters/discord/src/message.ts

--- adapters/discord/src/message.ts
+++ adapters/discord/src/message.ts
@@ -94,6 +94,7 @@ export class DiscordMessenger extends Messenger<DiscordBot> {
   async visit(element: segment) {
     const { type, attrs, children } = element
     if (type === 'text') {
+      // https://github.com/satorijs/satori/issues/51
       this.buffer += attrs.content.replace(/[\\*`~|()]/g, '\\$&')
     } else if (type === 'b' || type === 'strong') {
       this.buffer += '**'
