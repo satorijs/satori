3baea5bd745a922b811ca2240b0a59405b1af4c0
idanran <96647698+idanran@users.noreply.github.com>
2023-03-12 15:28:51 +0800

    Update index.ts

--- packages/axios/src/index.ts
+++ packages/axios/src/index.ts
@@ -86,9 +86,13 @@ export class Quester {
       const name = 'file' + (ext ? '.' + ext : '')
       return { mime, filename: name, data: base64ToArrayBuffer(base64) }
     }
-    const [_, name] = this.resolve(url).match(/.+\/([^/]*)/)
+    let [_, name] = this.resolve(url).match(/.+\/([^/]*)(?=\?)/)
     const { headers, data } = await this.axios(url, { method: 'GET', responseType: 'arraybuffer' })
     const mime = headers['content-type']
+    if (!name.includes('.')){
+      const ext = mimedb[mime]?.extensions?.[0]
+      name += ext ? '.' + ext : ''
+    }
     return { mime, filename: name, data }
   }
 }
