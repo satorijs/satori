4585662ace8373019ac5b05abf83687022e3d336
YiJie <51358815+NWYLZW@users.noreply.github.com>
2023-03-26 15:15:07 +0800

    fix(qqguild): support private message, fix koishijs/koishi#965 (#79)

--- adapters/qqguild/src/bot.ts
+++ adapters/qqguild/src/bot.ts
@@ -21,7 +21,21 @@ export class QQGuildBot extends Bot<QQGuildBot.Config> {
   }
 
   async sendMessage(channelId: string, fragment: Fragment, guildId?: string, opts?: SendOptions) {
-    return await new QQGuildMessenger(this, channelId, guildId, opts).send(fragment)
+    const messenger = new QQGuildMessenger(this, channelId, guildId, opts)
+    try {
+      return await messenger.send(fragment)
+    } catch (e) {
+      // https://bot.q.qq.com/wiki/develop/api/openapi/error/error.html#%E9%94%99%E8%AF%AF%E7%A0%81%E5%A4%84%E7%90%86:~:text=304031,%E6%8B%89%E7%A7%81%E4%BF%A1%E9%94%99%E8%AF%AF
+      if ([304031, 304032, 304033].includes(e.code)) {
+        await this.internal.createDMS(channelId, guildId)
+        return await messenger.send(fragment)
+      }
+      throw e
+    }
+  }
+
+  async sendPrivateMessage(userId: string, content: h.Fragment, options?: SendOptions): Promise<string[]> {
+    return this.sendMessage(userId, content, options.session.guildId, options)
   }
 
   async getGuildList() {
@@ -39,9 +53,18 @@ export class QQGuildBot extends Bot<QQGuildBot.Config> {
     })
     session.author = adaptUser(msg.author)
     session.userId = author.id
-    session.guildId = msg.guildId
-    session.channelId = msg.channelId
-    session.subtype = 'group'
+    // TODO https://github.com/satorijs/satori/blob/fbcf4665c77381ff80c8718106d2282a931d5736/packages/core/src/message.ts#L23
+    //      satori core need set guildId is undefined when isPrivate
+    //      this is a temporary solution
+    if (msg.isPrivate) {
+      session.guildId = undefined
+      session.channelId = msg.guildId
+    } else {
+      session.guildId = msg.guildId
+      session.channelId = msg.channelId
+    }
+    // it's useless, but I need it
+    session.subtype = msg.isPrivate ? 'private' : 'group'
     session.content = (msg.content ?? '')
       .replace(/<@!(.+)>/, (_, $1) => h.at($1).toString())
       .replace(/<#(.+)>/, (_, $1) => h.sharp($1).toString())
