4f6fdf8feee26328fa23b9193cb4cb88c97e3286
MODcraft <admin@ncgs.top>
2023-04-02 01:22:36 +0800

    更新频道API

--- adapters/kook/src/types.ts
+++ adapters/kook/src/types.ts
@@ -387,6 +387,47 @@ export interface Internal {
   createGuildMute(param: { guild_id: string; user_id: string; type: GuildMute.Type }): Promise<void>
   deleteGuildMute(param: { guild_id: string; user_id: string; type: GuildMute.Type }): Promise<void>
   getGuildBoostHistory(param: { guild_id: string; start_time: number; end_time: number }): Promise<List<GuildBoost>>
+
+  getChannelList(param: { guild_id: string }): Promise<List<Channel>>
+  getChannelView(param: { target_id: string }): Promise<Channel>
+  createChannel(param: {
+    guild_id: string
+    parent_id?: string
+    name: string
+    type?: number
+    limit_amount?: number
+    voice_quality?: string
+    is_category?: 0|1
+  }): Promise<Channel>
+  updateChannel(param: {
+    channel_id: string
+    name?: string
+    topic?: string
+    slow_mode?: 0|5000|10000|15000|30000|60000|120000|300000|600000|900000|1800000|3600000|7200000|21600000
+  }): Promise<Channel>
+  deleteChannel(param: { channel_id: string }): Promise<void>
+  getChannelUserList(param: { channel_id: string }): Promise<List<User>>
+  moveChannelUser(param: { target_id: string; user_ids: [] }): Promise<void>
+  getChannelRoleIndex(param: { channel_id: string }): Promise<{ permission_overwrites: Overwrite; permission_users: List<User>; permission_sync: 0 | 1 }>
+  createChannelRole(param: { channel_id: string; type?: 'role_id' | 'user_id'; value?: string }): Promise<{
+    user_id?: string // 此处待修改
+    role_id?: string // 此处待修改
+    allow: number
+    deny: number
+  }>
+  updateChannelRole(param: {
+    channel_id: string
+    type?: 'role_id' | 'user_id'
+    value?: string
+    allow?: number
+    deny?: number
+  }): Promise<{
+    user_id?: string // 此处待修改
+    role_id?: string // 此处待修改
+    allow: number
+    deny: number
+  }>
+  deleteChannelRole(param: { channel_id: string; type?: 'role_id' | 'user_id'; value?: string }): Promise<void>
 }
 
 export class Internal {
