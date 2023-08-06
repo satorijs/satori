be95c646ad7cacf72c8e73597d1e432edc16fd1d
LittleC <i@ltlec.com>
2023-08-07 00:18:30 +0800

    feat(wecom): add adapter-wecom

---
+++ adapters/wecom/src/message.ts
@@ -0,0 +1,111 @@
+import { h, MessageEncoder } from '@satorijs/satori'
+import { WecomBot } from './bot'
+import FormData from 'form-data'
+
+/** https://developer.work.weixin.qq.com/document/path/90236#%E6%94%AF%E6%8C%81%E7%9A%84markdown%E8%AF%AD%E6%B3%95 */
+
+export class WecomMessageEncoder extends MessageEncoder<WecomBot> {
+  buffer = ''
+  upsertSend(msgId: string) {
+    const session = this.bot.session()
+    session.type = 'message'
+    session.messageId = msgId
+    session.isDirect = true
+    session.userId = this.bot.selfId
+    session.timestamp = new Date().valueOf()
+    session.app.emit(session, 'send', session)
+    this.results.push(session)
+  }
+
+  /** https://developer.work.weixin.qq.com/document/path/90236 */
+  async sendByCustom(payload: any) {
+    if (payload.msgtype === 'text' && !payload.text?.content) return
+    // if (payload.msgtype === "markdown" && !payload.markdown?.content) return;
+    const { msgid } = await this.bot.http.post('/cgi-bin/message/send', {
+      touser: this.options.session.userId,
+      agentid: this.bot.selfId,
+      ...payload,
+    }, {
+      params: { access_token: this.bot.token },
+    })
+
+    this.upsertSend(msgid)
+  }
+
+  async flushMedia(element: h) {
+    if (!['audio', 'video', 'image', 'file'].includes(element.type)) return
+    let type = element.type
+    if (type === 'audio') type = 'voice'
+    const [media] = await this.uploadMedia(element)
+
+    await this.sendByCustom({
+      msgtype: type,
+      [type]: {
+        media_id: media,
+      },
+    })
+  }
+
+  async flush(): Promise<void> {
+    await this.sendByCustom({
+      msgtype: 'text',
+      text: {
+        content: this.buffer,
+      },
+    })
+    this.buffer = ''
+  }
+
+  /** https://developer.work.weixin.qq.com/document/path/90253 */
+  async uploadMedia(element: h) {
+    const { type, attrs } = element
+    const uploadType = type === 'audio' ? 'voice' : type
+    const form = new FormData()
+
+    const { filename, data, mime } = await this.bot.ctx.http.file(attrs.url, attrs)
+    const value = process.env.KOISHI_ENV === 'browser'
+      ? new Blob([data], { type: mime })
+      : Buffer.from(data)
+
+    form.append('media', value, attrs.file || filename)
+
+    const resp = await this.bot.http.post<{
+      type: string
+      media_id: string
+      created_at: number
+      errcode: number
+      errmsg: string
+    }>('/cgi-bin/media/upload', form, {
+      params: {
+        access_token: this.bot.token,
+        type: uploadType,
+      },
+      headers: form.getHeaders(),
+    })
+    if (resp.media_id) {
+      return [resp.media_id, uploadType]
+    }
+    this.bot.logger.error(resp.errmsg)
+  }
+
+  async visit(element: h) {
+    const { type, attrs, children } = element
+    if (type === 'text') {
+      this.buffer += attrs.content
+    } else if (type === 'p') {
+      await this.render(children)
+      this.buffer += '\n'
+    } else if (type === 'image' || type === 'audio' || type === 'video' || type === 'file') {
+      await this.flushMedia(element)
+    } else if (type === 'a' && attrs.href) {
+      await this.render(children)
+      this.buffer += ` (${attrs.href})`
+    } else if (type === 'message') {
+      await this.flush()
+      await this.render(children)
+      await this.flush()
+    } else {
+      await this.render(children)
+    }
+  }
+}
