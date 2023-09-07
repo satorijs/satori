satorijs/satori

commit 8f06f3bfeb076056666ca77e5a55d4760a017d2d

fix(telegram): polling when stops bot (#150)
Co-authored-by: Shigma <shigma10826@gmail.com>

adapters/telegram/src/polling.ts
@@ -6,6 +6,7 @@ const logger = new Logger('telegram')
 
 export class HttpPolling extends Adapter.Client<TelegramBot> {
   private offset = 0
+  private timeout: NodeJS.Timeout
 
   async start(bot: TelegramBot<TelegramBot.BaseConfig & HttpPolling.Config>) {
     bot.initialize(async () => {
@@ -28,15 +29,14 @@ export class HttpPolling extends Adapter.Client<TelegramBot> {
       })
 
       const polling = async () => {
-        if (bot.status === 'disconnect') {
-          return bot.offline()
-        }
-
         try {
           const updates = await bot.internal.getUpdates({
             offset: this.offset + 1,
             timeout: Math.ceil(bot.config.pollingTimeout / 1000), // in seconds
           })
+          if (bot.status === 'offline' || bot.status === 'disconnect') {
+            return
+          }
           bot.online()
           _retryCount = 0
           _initial = false
@@ -45,7 +45,7 @@ export class HttpPolling extends Adapter.Client<TelegramBot> {
             this.offset = Math.max(this.offset, e.update_id)
             handleUpdate(e, bot)
           }
-          setTimeout(polling, 0)
+          this.timeout = setTimeout(polling, 0)
         } catch (e) {
           if (!Quester.isAxiosError(e) || !e.response?.data) {
             // Other error
@@ -60,9 +60,12 @@ export class HttpPolling extends Adapter.Client<TelegramBot> {
             bot.error = e
             return bot.status = 'offline'
           }
+          if (bot.status === 'offline' || bot.status === 'disconnect') {
+            return
+          }
           _retryCount++
           bot.status = 'reconnect'
-          setTimeout(() => polling(), retryInterval)
+          this.timeout = setTimeout(polling, retryInterval)
         }
       }
       polling()
@@ -70,7 +73,9 @@ export class HttpPolling extends Adapter.Client<TelegramBot> {
     })
   }
 
-  async stop() {}
+  async stop() {
+    clearTimeout(this.timeout)
+  }
 }
 
 export namespace HttpPolling {

packages/core/src/bot.ts
@@ -94,6 +94,7 @@ export abstract class Bot<T extends Bot.Config = Bot.Config> {
       await this.adapter?.stop(this)
     } catch (error) {
       this.context.emit('internal/warning', error)
+    } finally {
       this.offline()
     }
   }

commit 15259579d29ebca70982201e99f5233bf1e6b73c

fix(core): support `bot.isActive`, fix reconnect, close #153
regression by 8f06f3bfeb076056666ca77e5a55d4760a017d2d

adapters/discord/src/ws.ts
@@ -70,7 +70,6 @@ export class WsClient extends Adapter.WsClient<DiscordBot> {
         if (parsed.d) return
         this._sessionId = ''
         logger.warn('offline: invalid session')
-        this.bot.offline()
         this.bot.socket?.close()
       }
 
@@ -92,7 +91,6 @@ export class WsClient extends Adapter.WsClient<DiscordBot> {
       }
 
       if (parsed.op === Gateway.Opcode.RECONNECT) {
-        this.bot.offline()
         logger.warn('offline: discord request reconnect')
         this.bot.socket?.close()
       }

adapters/mail/src/bot.ts
@@ -44,17 +44,11 @@ export class MailBot extends Bot<MailBot.Config> {
   }
 
   onClose() {
-    if (this.status === 'disconnect') {
-      this.offline()
-      return
-    }
+    if (!this.isActive) return
     logger.info('IMAP disconnected, will reconnect in 3s...')
     this.status = 'reconnect'
     setTimeout(() => {
-      if (this.status !== 'reconnect') {
-        this.offline()
-        return
-      }
+      if (!this.isActive) return
       this.imap.connect()
     }, 3000)
   }

adapters/telegram/src/polling.ts
@@ -34,9 +34,7 @@ export class HttpPolling extends Adapter.Client<TelegramBot> {
             offset: this.offset + 1,
             timeout: Math.ceil(bot.config.pollingTimeout / 1000), // in seconds
           })
-          if (bot.status === 'offline' || bot.status === 'disconnect') {
-            return
-          }
+          if (!bot.isActive) return
           bot.online()
           _retryCount = 0
           _initial = false
@@ -60,9 +58,7 @@ export class HttpPolling extends Adapter.Client<TelegramBot> {
             bot.error = e
             return bot.status = 'offline'
           }
-          if (bot.status === 'offline' || bot.status === 'disconnect') {
-            return
-          }
+          if (!bot.isActive) return
           _retryCount++
           bot.status = 'reconnect'
           this.timeout = setTimeout(polling, retryInterval)

packages/core/src/adapter.ts
@@ -73,9 +73,7 @@ export namespace Adapter {
         socket.addEventListener('close', ({ code, reason }) => {
           bot.socket = null
           logger.debug(`websocket closed with ${code}`)
-          if (bot.status === 'disconnect') {
-            return bot.status = 'offline'
-          }
+          if (!bot.isActive) return
 
           const message = reason.toString() || `failed to connect to ${url}, code: ${code}`
           let timeout = retryInterval

packages/core/src/bot.ts
@@ -65,6 +65,10 @@ export abstract class Bot<T extends Bot.Config = Bot.Config> {
     }
   }
 
+  get isActive() {
+    return this._status !== 'offline' && this._status !== 'disconnect'
+  }
+
   online() {
     this.status = 'online'
     this.error = null
@@ -76,7 +80,7 @@ export abstract class Bot<T extends Bot.Config = Bot.Config> {
   }
 
   async start() {
-    if (['connect', 'reconnect', 'online'].includes(this.status)) return
+    if (this.isActive) return
     this.status = 'connect'
     try {
       await this.context.parallel('bot-connect', this)
@@ -87,7 +91,7 @@ export abstract class Bot<T extends Bot.Config = Bot.Config> {
   }
 
   async stop() {
-    if (['disconnect', 'offline'].includes(this.status)) return
+    if (!this.isActive) return
     this.status = 'disconnect'
     try {
       await this.context.parallel('bot-disconnect', this)

packages/core/src/session.ts
@@ -53,6 +53,7 @@ export class Session {
     }
     this.selfId = bot.selfId
     this.platform = bot.platform
+    this.locales = []
     defineProperty(this, 'bot', bot)
     defineProperty(this, 'app', bot.ctx.root)
     defineProperty(this, 'id', ++bot.ctx.internal.counter)