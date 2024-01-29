1bd24acb323058e7fd391a4e7d37117a066e7380
_LittleC_ <26459759+XxLittleCxX@users.noreply.github.com>
2024-01-29 22:50:53 +0800

    feat(qq): add `event-id` support to `<passive>` (#234)

--- adapters/qq/src/message.ts
+++ adapters/qq/src/message.ts
@@ -15,6 +15,7 @@ export class QQGuildMessageEncoder<C extends Context = Context> extends MessageE
   private filename: string
   fileUrl: string
   private passiveId: string
+  private passiveEventId: string
   reference: string
   private retry = false
   // 先文后图
@@ -42,6 +43,9 @@ export class QQGuildMessageEncoder<C extends Context = Context> extends MessageE
         if (this.options?.session && msg_id) {
           form.append('msg_id', msg_id)
         }
+        if (this.passiveEventId) {
+          form.append('event_id', this.passiveEventId)
+        }
         if (this.file) {
           form.append('file_image', this.file, this.filename)
         }
@@ -62,6 +66,9 @@ export class QQGuildMessageEncoder<C extends Context = Context> extends MessageE
             message_reference: {
               message_id: this.reference,
             },
+          } : {}),
+          ...(this.passiveEventId ? {
+            event_id: this.passiveEventId,
           } : {}),
         }
         if (isDirect) r = await this.bot.internal.sendDM(this.channelId.split('_')[0], payload)
@@ -167,7 +174,8 @@ export class QQGuildMessageEncoder<C extends Context = Context> extends MessageE
       this.reference = attrs.id
       await this.flush()
     } else if (type === 'passive') {
-      this.passiveId = attrs.id
+      if (attrs.messageId) this.passiveId = attrs.messageId
+      if (attrs.eventId) this.passiveEventId = attrs.eventId
     } else if ((type === 'img' || type === 'image') && (attrs.src || attrs.url)) {
       await this.flush()
       await this.resolveFile(attrs)
@@ -381,7 +389,7 @@ export class QQMessageEncoder<C extends Context = Context> extends MessageEncode
     if (type === 'text') {
       this.content += attrs.content
     } else if (type === 'passive') {
-      this.passiveId = attrs.id
+      this.passiveId = attrs.messageId
       this.passiveSeq = Number(attrs.seq)
     } else if ((type === 'img' || type === 'image') && (attrs.src || attrs.url)) {
       await this.flush()
