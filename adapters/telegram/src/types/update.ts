71b09f8db347b5b9a8fec6441fa96f0d0b6f8dad
Shigma <shigma10826@gmail.com>
2025-03-23 14:08:42 +0800

    chore: bump versions

--- adapters/telegram/src/types/update.ts
+++ adapters/telegram/src/types/update.ts
@@ -6,7 +6,7 @@ import { InputFile, Integer, Internal } from '.'
  */
 export interface Update {
   /** The update's unique identifier. Update identifiers start from a certain positive number and increase sequentially. This ID becomes especially handy if you're using Webhooks, since it allows you to ignore repeated updates or to restore the correct update sequence, should they get out of order. If there are no new updates for at least a week, then identifier of the next update will be chosen randomly instead of sequentially. */
-  update_id?: Integer
+  update_id: Integer
 }
 
 export interface GetUpdatesPayload {
