0cf9d1fd9af2b4cb9d4e7f7bd0606ab20a335bc7
_LittleC_ <26459759+XxLittleCxX@users.noreply.github.com>
2025-05-25 23:46:08 +0800

    feat(qq): support gateway url override (#354)

--- adapters/qq/src/bot/index.ts
+++ adapters/qq/src/bot/index.ts
@@ -125,6 +125,7 @@ export namespace QQBot {
     manualAcknowledge: boolean
     protocol: 'websocket' | 'webhook'
     path?: string
+    gatewayUrl?: string
   }
 
   export type Config = BaseConfig & (HttpServer.Options | WsClient.Options)
@@ -148,6 +149,7 @@ export namespace QQBot {
     ]),
     Schema.object({
       manualAcknowledge: Schema.boolean().description('手动响应回调消息。').default(false),
+      gatewayUrl: Schema.string().role('link').description('覆写 WebSocket 地址。'),
     }).description('高级设置'),
   ] as const)
 }
