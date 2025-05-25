6b3e15c2750a0052c047f562eced25362465cd54
ForkΨKillet <35412022+ForkKILLET@users.noreply.github.com>
2025-05-26 00:18:35 +0800

    fix(element): JSX children prop typings (#353)

--- packages/element/src/index.ts
+++ packages/element/src/index.ts
@@ -7,13 +7,13 @@ declare global {
       message: {
         id?: string
         forward?: boolean
-        children?: any[]
+        children?: any
       }
       quote: {
         id?: string
         name?: string
         avatar?: string
-        children?: any[]
+        children?: any
       }
       at: {
         id?: string
