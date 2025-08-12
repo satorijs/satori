70d6e16d50afa632625eb0a815cc3a446e3b0dc7
Maiko Tan <maiko.tan.coding@gmail.com>
2025-08-12 16:02:04 +0800

    feat: add generate script for Internal types

--- adapters/heychat/src/types/upload.ts
+++ adapters/heychat/src/types/upload.ts
@@ -1,1 +1,17 @@
-export {}
+import { Internal } from '../internal'
+import FormData from 'form-data'
+
+/**
+ * 上传媒体文件
+ */
+
+declare module '../internal' {
+  interface Internal {
+    postUpload: (data: FormData) => Promise<void>
+  }
+}
+Internal.define({
+  '/upload': {
+    POST: 'postUpload',
+  },
+})
