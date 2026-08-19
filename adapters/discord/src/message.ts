d3d2f4a51206b577a9e38b451bc0e47de969eabc
Hieuzest <girkirin@hotmail.com>
2026-08-19 15:44:04 +0800

    refactor(discord): use url-is-local package

--- adapters/discord/src/message.ts
+++ adapters/discord/src/message.ts
@@ -1,5 +1,6 @@
-import { Dict, h, isLocal, MessageEncoder, Universal } from '@satorijs/core'
+import { Dict, h, MessageEncoder, Universal } from '@satorijs/core'
 import {} from '@cordisjs/plugin-http'
+import isLocal from '@cordisjs/url-is-local'
 import { DiscordBot } from './bot'
 import { ActionRow, Button, ButtonStyles, Channel, ComponentType, Message } from './types'
 import { decodeMessage, downloadFile, sanitize, sanitizeCode } from './utils'
